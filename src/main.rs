76f108b997b2d9a83c432fd94e66ad7aed07285c
Roy van de Water <roy.v.water@gmail.com>
2023-11-10 11:59:12 -0700

    Hello purple circle
    Co-authored-by: Aaron Herres <iam@aaronherres.com>

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
 use bevy::prelude::*;
+use bevy::sprite::MaterialMesh2dBundle;
 
 fn main() {
     App::new()
-        .add_systems(Startup, add_people)
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, setup)
         .add_systems(Update, greet_things_with_names)
         .run();
-}
-
-// component
-#[derive(Component)]
-struct Position {
-    x: f32,
-    y: f32,
 }
 
 #[derive(Component)]
@@ -27,8 +22,21 @@ fn greet_things_with_names(query: Query<&Name>) {
     }
 }
 
-fn add_people(mut commands: Commands) {
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    commands.spawn(Camera2dBundle::default());
     commands.spawn((Person, Name("Elaina Proctor".to_string())));
     commands.spawn((Person, Name("Renzo Hume".to_string())));
     commands.spawn((Person, Name("Zayna Nieves".to_string())));
+
+    // Circle
+    commands.spawn(MaterialMesh2dBundle {
+        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+        material: materials.add(ColorMaterial::from(Color::PURPLE)),
+        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+        ..default()
+    });
 }
