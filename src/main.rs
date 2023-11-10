5c5197d891f02caf468f26c81c9532f0d0c6d763
Roy van de Water <roy.v.water@gmail.com>
2023-11-10 12:30:07 -0700

    Click shoots, bullet (technically) flies
    Co-authored-by: Aaron Herres <iam@aaronherres.com>

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
         .add_systems(Update, keyboard_input_system)
+        .add_systems(Update, mouse_click_system)
+        .add_systems(Update, bullet_moves_forward_system)
         .run();
 }
 
@@ -24,6 +26,15 @@ struct PlayerBundle {
     mesh_bundle: MaterialMesh2dBundle<ColorMaterial>,
 }
 
+#[derive(Component)]
+struct Bullet;
+
+#[derive(Bundle)]
+struct BulletBundle {
+    bullet: Bullet,
+    mesh_bundle: MaterialMesh2dBundle<ColorMaterial>,
+}
+
 fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
@@ -37,7 +48,7 @@ fn setup(
         mesh_bundle: MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::new(50.).into()).into(),
             material: materials.add(ColorMaterial::from(Color::PURPLE)),
-            transform: Transform::from_translation(Vec3::new(0., 25., 0.)),
+            transform: Transform::from_translation(Vec3::new(0., 50., 0.)),
             ..default()
         },
     });
@@ -51,6 +62,12 @@ fn setup(
         transform: Transform::from_translation(Vec3::new(0., -500., -0.1)),
         ..default()
     });
+}
+
+fn bullet_moves_forward_system(mut bullets: Query<&mut Transform, With<Bullet>>) {
+    for mut bullet in bullets.iter_mut() {
+        bullet.translation.y += 1.;
+    }
 }
 
 // keyboard
@@ -67,5 +84,27 @@ fn keyboard_input_system(
 
     if keyboard_input.pressed(KeyCode::D) {
         player.translation.x += 1.;
+    }
+}
+
+// This system prints messages when you press or release the left mouse button:
+fn mouse_click_system(
+    mut commands: Commands,
+    mouse_button_input: Res<Input<MouseButton>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    if mouse_button_input.just_pressed(MouseButton::Left) {
+        commands.spawn(BulletBundle {
+            bullet: Bullet,
+            mesh_bundle: MaterialMesh2dBundle {
+                mesh: meshes
+                    .add(shape::Quad::new(Vec2::new(10., 10.)).into())
+                    .into(),
+                material: materials.add(ColorMaterial::from(Color::RED)),
+                transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+                ..default()
+            },
+        });
     }
 }
