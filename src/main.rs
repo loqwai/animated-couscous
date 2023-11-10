b2943e25ee7ef25f3ca0438aee19ba4ff873e9ce
Roy van de Water <roy.v.water@gmail.com>
2023-11-10 11:53:17 -0700

    Hello Elaina!
    Co-authored-by: Aaron Herres <iam@aaronherres.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_systems(Startup, add_people)
+        .add_systems(Update, greet_things_with_names)
+        .run();
+}
+
+// component
+#[derive(Component)]
+struct Position {
+    x: f32,
+    y: f32,
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+// system
+fn greet_things_with_names(query: Query<&Name>) {
+    for thing in &query {
+        println!("Hi {}", thing.0);
+    }
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Elaina Proctor".to_string())));
+    commands.spawn((Person, Name("Renzo Hume".to_string())));
+    commands.spawn((Person, Name("Zayna Nieves".to_string())));
 }
