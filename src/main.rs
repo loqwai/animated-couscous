4f8987558a4c39fdf36f730c8ec359322f416bd4
Roy van de Water <roy.v.water@gmail.com>
2023-11-10 12:11:28 -0700

    Purple circle go left
    Co-authored-by: Aaron Herres <iam@aaronherres.com>

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, greet_things_with_names)
+        .add_systems(Update, keyboard_input_system)
         .run();
 }
 
@@ -15,11 +15,13 @@ struct Person;
 #[derive(Component)]
 struct Name(String);
 
-// system
-fn greet_things_with_names(query: Query<&Name>) {
-    for thing in &query {
-        println!("Hi {}", thing.0);
-    }
+#[derive(Component)]
+struct Player;
+
+#[derive(Bundle)]
+struct PlayerBundle {
+    player: Player,
+    mesh_bundle: MaterialMesh2dBundle<ColorMaterial>,
 }
 
 fn setup(
@@ -28,15 +30,42 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
     commands.spawn(Camera2dBundle::default());
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
 
-    // Circle
+    // Player
+    commands.spawn(PlayerBundle {
+        player: Player,
+        mesh_bundle: MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            transform: Transform::from_translation(Vec3::new(0., 25., 0.)),
+            ..default()
+        },
+    });
+
+    // Ground
     commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-        material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+        mesh: meshes
+            .add(shape::Quad::new(Vec2::new(1000., 1000.)).into())
+            .into(),
+        material: materials.add(ColorMaterial::from(Color::DARK_GREEN)),
+        transform: Transform::from_translation(Vec3::new(0., -500., -0.1)),
         ..default()
     });
+}
+
+// keyboard
+/// This system prints 'A' key state
+fn keyboard_input_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut players: Query<&mut Transform, With<Player>>,
+) {
+    let mut player = players.get_single_mut().unwrap();
+
+    if keyboard_input.pressed(KeyCode::A) {
+        player.translation.x -= 1.;
+    }
+
+    if keyboard_input.pressed(KeyCode::D) {
+        player.translation.x += 1.;
+    }
 }
