b346f367c7bd716dba2f92a7c2a91b6ea90c6491
Roy van de Water <roy.v.water@gmail.com>
2023-11-10 13:26:36 -0700

    Guns don't kill people, bullets do
    Co-authored-by: Aaron Herres <iam@aaronherres.com>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 use bevy::window::PrimaryWindow;
+use rand::prelude::*;
 
 fn main() {
     App::new()
@@ -9,6 +10,8 @@ fn main() {
         .add_systems(Update, keyboard_input_system)
         .add_systems(Update, mouse_click_system)
         .add_systems(Update, bullet_moves_forward_system)
+        .add_systems(Update, ensure_dummy)
+        .add_systems(Update, bullet_hit_despawns_dummy)
         .run();
 }
 
@@ -20,6 +23,17 @@ struct Name(String);
 
 #[derive(Component)]
 struct Player;
+
+// dummy component
+#[derive(Component)]
+struct Dummy;
+
+#[derive(Bundle)]
+struct DummyBundle {
+    dummy: Dummy,
+    name: Name,
+    mesh_bundle: MaterialMesh2dBundle<ColorMaterial>,
+}
 
 #[derive(Bundle)]
 struct PlayerBundle {
@@ -48,7 +62,7 @@ fn setup(
         player: Player,
         mesh_bundle: MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-            material: materials.add(ColorMaterial::from(Color::PURPLE)),
+            material: materials.add(ColorMaterial::from(Color::BLUE)),
             transform: Transform::from_translation(Vec3::new(0., 50., 0.)),
             ..default()
         },
@@ -59,7 +73,7 @@ fn setup(
         mesh: meshes
             .add(shape::Quad::new(Vec2::new(1000., 1000.)).into())
             .into(),
-        material: materials.add(ColorMaterial::from(Color::DARK_GREEN)),
+        material: materials.add(ColorMaterial::from(Color::GRAY)),
         transform: Transform::from_translation(Vec3::new(0., -500., -0.1)),
         ..default()
     });
@@ -68,7 +82,7 @@ fn setup(
 fn bullet_moves_forward_system(mut bullets: Query<&mut Transform, With<Bullet>>) {
     for mut bullet in bullets.iter_mut() {
         // move bullet forward, taking it's rotation into account
-        let rotation = bullet.rotation * Vec3::X;
+        let rotation = bullet.rotation * Vec3::X * 5.;
         bullet.translation += rotation;
     }
 }
@@ -129,6 +143,8 @@ fn mouse_click_system_fallible(
     if mouse_button_input.just_pressed(MouseButton::Left) {
         let ray = cursor_position - player.translation;
         let rotation = Quat::from_rotation_z(ray.y.atan2(ray.x));
+        let mut transform = player.clone().with_rotation(rotation);
+        transform.translation.z += 0.1;
 
         commands.spawn(BulletBundle {
             bullet: Bullet,
@@ -136,11 +152,52 @@ fn mouse_click_system_fallible(
                 mesh: meshes
                     .add(shape::Quad::new(Vec2::new(40., 10.)).into())
                     .into(),
-                material: materials.add(ColorMaterial::from(Color::RED)),
-                transform: player.clone().with_rotation(rotation),
+                material: materials.add(ColorMaterial::from(Color::WHITE)),
+                transform,
                 ..default()
             },
         });
     }
     return Some(());
+}
+
+fn ensure_dummy(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    dummies: Query<&Name, With<Dummy>>,
+) {
+    if dummies.iter().count() == 0 {
+        let mut rng = rand::thread_rng();
+        let mut random_position: f32 = rng.gen();
+        random_position *= 1000.;
+        random_position -= 500.;
+
+        commands.spawn(DummyBundle {
+            dummy: Dummy,
+            name: Name("Dummy".to_string()),
+            mesh_bundle: MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+                material: materials.add(ColorMaterial::from(Color::RED)),
+                transform: Transform::from_translation(Vec3::new(random_position, 50., -0.1)),
+                ..default()
+            },
+        });
+    }
+}
+
+fn bullet_hit_despawns_dummy(
+    mut commands: Commands,
+    bullets: Query<(Entity, &Transform), With<Bullet>>,
+    mut dummies: Query<(Entity, &Transform), With<Dummy>>,
+) {
+    for (bullet, bloc) in bullets.iter() {
+        for (entity, dummy) in dummies.iter_mut() {
+            if bloc.translation.distance(dummy.translation) < 70. {
+                commands.entity(entity).despawn();
+                commands.entity(bullet).despawn();
+            }
+        }
+        // despawn the bullet
+    }
 }
