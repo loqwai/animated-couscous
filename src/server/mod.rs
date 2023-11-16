3a40bf0467f2bb81e0d4711895c8cb443b77a205
Roy van de Water <roy.v.water@gmail.com>
2023-11-16 09:55:21 -0700

    Simplification and graceful handling of disconnects

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -1,71 +1,73 @@
 use std::{
     net::{TcpListener, TcpStream},
     thread,
-    time::Duration,
 };
 
-use crossbeam_channel::{Receiver, Sender};
+use crossbeam_channel::Sender;
 use protobuf::{CodedInputStream, Message};
 
 use crate::protos::generated::applesauce;
 
 enum Event {
-    Streams(Vec<TcpStream>),
+    Disconnect(TcpStream),
+    Connect(TcpStream),
     Input(applesauce::Input),
 }
 
 pub(crate) fn serve(listener: TcpListener) {
     let (tx, rx) = crossbeam_channel::bounded::<Event>(10);
 
+    let rx2 = rx.clone();
+
+    thread::spawn(move || {
+        for stream in listener.incoming() {
+            let stream = stream.unwrap();
+
+            let events_tx = tx.clone();
+
+            thread::spawn(move || {
+                handle_connection(stream, events_tx);
+            });
+        }
+    });
+
     thread::spawn(move || {
         let mut streams: Vec<TcpStream> = vec![];
 
-        for stream in listener.incoming() {
-            let stream = stream.unwrap();
-            streams.push(stream.try_clone().unwrap());
-            let events_tx = tx.clone();
-            let events_rx = rx.clone();
-
-            thread::spawn(move || {
-                handle_connection(stream, events_tx, events_rx);
-            });
-
-            thread::sleep(Duration::from_millis(10));
-            tx.send(Event::Streams(clone_streams(&streams))).unwrap();
-        }
-    });
-}
-
-fn clone_streams(streams: &Vec<TcpStream>) -> Vec<TcpStream> {
-    let mut new_streams: Vec<TcpStream> = vec![];
-
-    for stream in streams.iter() {
-        new_streams.push(stream.try_clone().unwrap());
-    }
-
-    new_streams
-}
-
-fn handle_connection(mut stream: TcpStream, events_tx: Sender<Event>, events_rx: Receiver<Event>) {
-    thread::spawn(move || loop {
-        let mut input_stream = CodedInputStream::new(&mut stream);
-        if input_stream.eof().unwrap() {
-            return;
-        }
-
-        let input: applesauce::Input = input_stream.read_message().unwrap();
-        events_tx.send(Event::Input(input)).unwrap();
-    });
-
-    let mut streams: Vec<TcpStream> = vec![];
-    for event in events_rx.iter() {
-        match event {
-            Event::Streams(new_streams) => streams = new_streams,
-            Event::Input(input) => {
-                for mut stream in streams.iter() {
-                    input.write_length_delimited_to_writer(&mut stream).unwrap();
+        for event in rx2.iter() {
+            match event {
+                Event::Disconnect(stream) => {
+                    streams.retain_mut(|s| s.peer_addr().unwrap() != stream.peer_addr().unwrap());
+                }
+                Event::Connect(stream) => {
+                    streams.push(stream);
+                }
+                Event::Input(input) => {
+                    for mut stream in streams.iter() {
+                        input.write_length_delimited_to_writer(&mut stream).unwrap();
+                    }
                 }
             }
         }
-    }
+    });
+}
+
+fn handle_connection(stream: TcpStream, events_tx: Sender<Event>) {
+    events_tx
+        .send(Event::Connect(stream.try_clone().unwrap()))
+        .unwrap();
+
+    thread::spawn(move || {
+        let mut input_stream = stream.try_clone().unwrap();
+        let mut input_stream = CodedInputStream::new(&mut input_stream);
+        loop {
+            if input_stream.eof().unwrap() {
+                events_tx.send(Event::Disconnect(stream)).unwrap();
+                return;
+            }
+
+            let input: applesauce::Input = input_stream.read_message().unwrap();
+            events_tx.send(Event::Input(input)).unwrap();
+        }
+    });
 }
