d337fd75bb23b59d002367243e5556ee213df5a7
Roy van de Water <roy.v.water@gmail.com>
2023-11-17 13:28:20 -0700

    Two players act separately

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 mod protos;
 mod server;
 
+use std::time::Duration;
+
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 use bevy::window::WindowPlugin;
 use bevy::window::{PrimaryWindow, WindowResolution};
 use crossbeam_channel::{Receiver, Sender};
+use protos::generated::applesauce::wrapper::Inner;
 use rand::prelude::*;
 
 use protos::generated::applesauce;
@@ -31,26 +34,31 @@ fn main() {
             ..Default::default()
         }))
         .add_event::<InputEvent>()
+        .add_event::<PlayerSpawnEvent>()
         .add_systems(Startup, setup)
         .add_systems(Startup, start_local_server)
         .add_systems(Update, move_player)
         .add_systems(Update, maybe_fire_bullet)
         .add_systems(Update, bullet_moves_forward_system)
         .add_systems(Update, ensure_dummy)
+        .add_systems(Update, spawn_player)
         .add_systems(Update, bullet_hit_despawns_dummy)
         .add_systems(Update, write_inputs_to_server)
-        .add_systems(Update, broadcast_incoming_events)
+        .add_systems(Update, incoming_network_messages_to_events)
+        .add_systems(Update, broadcast_state)
         .run();
 }
-
-#[derive(Component)]
-struct Person;
 
 #[derive(Component)]
 struct Name(String);
 
 #[derive(Component)]
-struct Player;
+struct Player {
+    id: String,
+}
+
+#[derive(Component)]
+struct MainPlayer;
 
 // dummy component
 #[derive(Component)]
@@ -69,6 +77,12 @@ struct PlayerBundle {
     mesh_bundle: MaterialMesh2dBundle<ColorMaterial>,
 }
 
+#[derive(Bundle)]
+struct MainPlayerBundle {
+    main_player: MainPlayer,
+    player_bundle: PlayerBundle,
+}
+
 #[derive(Component)]
 struct Bullet;
 
@@ -80,18 +94,27 @@ struct BulletBundle {
 
 #[derive(Resource)]
 struct NetServer {
-    tx: Sender<applesauce::Input>,
-    rx: Receiver<applesauce::Input>,
+    tx: Sender<applesauce::Wrapper>,
+    rx: Receiver<applesauce::Wrapper>,
+    poll_timer: Timer,
 }
 
 #[derive(Event)]
 struct InputEvent {
+    player_id: String,
+
     move_left: bool,
     move_right: bool,
 
     aim_x: f32,
     aim_y: f32,
     fire_button_pressed: bool,
+}
+
+#[derive(Event)]
+struct PlayerSpawnEvent {
+    player_id: String,
+    position: Vec3,
 }
 
 fn setup(
@@ -102,13 +125,18 @@ fn setup(
     commands.spawn(Camera2dBundle::default());
 
     // Player
-    commands.spawn(PlayerBundle {
-        player: Player,
-        mesh_bundle: MaterialMesh2dBundle {
-            mesh: meshes.add(shape::Circle::new(50.).into()).into(),
-            material: materials.add(ColorMaterial::from(Color::BLUE)),
-            transform: Transform::from_translation(Vec3::new(0., 50., 0.)),
-            ..default()
+    commands.spawn(MainPlayerBundle {
+        main_player: MainPlayer,
+        player_bundle: PlayerBundle {
+            player: Player {
+                id: uuid::Uuid::new_v4().to_string(),
+            },
+            mesh_bundle: MaterialMesh2dBundle {
+                mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+                material: materials.add(ColorMaterial::from(Color::BLUE)),
+                transform: Transform::from_translation(Vec3::new(0., 50., 0.)),
+                ..default()
+            },
         },
     });
 
@@ -128,20 +156,45 @@ fn start_local_server(mut commands: Commands) {
     let connect_addr = std::env::var("CONNECT_TO").unwrap_or("localhost:3191".to_string());
 
     let (tx, rx) = server::serve(&listen_addr, &connect_addr);
+    let poll_timer = Timer::new(Duration::from_secs(1), TimerMode::Repeating);
 
-    commands.insert_resource(NetServer { tx, rx });
+    commands.insert_resource(NetServer { tx, rx, poll_timer });
 }
 
-fn broadcast_incoming_events(connection: ResMut<NetServer>, mut events: EventWriter<InputEvent>) {
+fn incoming_network_messages_to_events(
+    connection: ResMut<NetServer>,
+    mut input_events: EventWriter<InputEvent>,
+    mut player_spawn_events: EventWriter<PlayerSpawnEvent>,
+) {
     for input in connection.rx.try_iter() {
-        events.send(InputEvent {
-            move_left: input.move_left,
-            move_right: input.move_right,
+        match input.inner.unwrap() {
+            Inner::Input(input) => {
+                input_events.send(InputEvent {
+                    player_id: input.player_id,
 
-            aim_x: input.aim_x,
-            aim_y: input.aim_y,
-            fire_button_pressed: input.fire_button_pressed,
-        });
+                    move_left: input.move_left,
+                    move_right: input.move_right,
+
+                    aim_x: input.aim_x,
+                    aim_y: input.aim_y,
+                    fire_button_pressed: input.fire_button_pressed,
+                });
+            }
+            Inner::PlayerSpawn(player_spawn) => {
+                player_spawn_events.send(PlayerSpawnEvent {
+                    player_id: player_spawn.id,
+                    position: Vec3::new(player_spawn.x, player_spawn.y, 0.),
+                });
+            }
+            Inner::State(state) => {
+                for player_spawn in state.players.iter() {
+                    player_spawn_events.send(PlayerSpawnEvent {
+                        player_id: player_spawn.id.clone(),
+                        position: Vec3::new(player_spawn.x, player_spawn.y, 0.),
+                    });
+                }
+            }
+        }
     }
 }
 
@@ -155,19 +208,23 @@ fn bullet_moves_forward_system(mut bullets: Query<&mut Transform, With<Bullet>>)
 
 // keyboard
 /// This system prints 'A' key state
-fn move_player(
-    mut players: Query<&mut Transform, With<Player>>,
-    mut events: EventReader<InputEvent>,
-) {
-    let mut player = players.get_single_mut().unwrap();
-
+fn move_player(mut players: Query<(&Player, &mut Transform)>, mut events: EventReader<InputEvent>) {
     for event in events.read() {
-        if event.move_left {
-            player.translation.x -= 1.;
-        }
+        let player = players
+            .iter_mut()
+            .find(|(player, _)| player.id == event.player_id);
 
-        if event.move_right {
-            player.translation.x += 1.;
+        match player {
+            None => continue,
+            Some((_, mut transform)) => {
+                if event.move_left {
+                    transform.translation.x -= 1.;
+                }
+
+                if event.move_right {
+                    transform.translation.x += 1.;
+                }
+            }
         }
     }
 }
@@ -177,31 +234,39 @@ fn maybe_fire_bullet(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut events: EventReader<InputEvent>,
-    players: Query<&Transform, With<Player>>,
+    players: Query<(&Player, &Transform)>,
 ) {
-    let player = players.get_single().unwrap();
-
     for event in events.read() {
         if !event.fire_button_pressed {
             continue;
         }
 
-        let ray = Vec3::new(event.aim_x, event.aim_y, 0.);
-        let rotation = Quat::from_rotation_z(ray.y.atan2(ray.x));
-        let mut transform = player.clone().with_rotation(rotation);
-        transform.translation.z += 0.1;
+        match players.iter().find(|(p, _)| p.id == event.player_id) {
+            Some((_, transform)) => {
+                let ray = Vec3::new(event.aim_x, event.aim_y, 0.);
+                let rotation = Quat::from_rotation_z(ray.y.atan2(ray.x));
+                let mut transform = transform.clone().with_rotation(rotation);
+                transform.translation.z += 0.1;
 
-        commands.spawn(BulletBundle {
-            bullet: Bullet,
-            mesh_bundle: MaterialMesh2dBundle {
-                mesh: meshes
-                    .add(shape::Quad::new(Vec2::new(40., 10.)).into())
-                    .into(),
-                material: materials.add(ColorMaterial::from(Color::WHITE)),
-                transform,
-                ..default()
-            },
-        });
+                commands.spawn(BulletBundle {
+                    bullet: Bullet,
+                    mesh_bundle: MaterialMesh2dBundle {
+                        mesh: meshes
+                            .add(shape::Quad::new(Vec2::new(40., 10.)).into())
+                            .into(),
+                        material: materials.add(ColorMaterial::from(Color::WHITE)),
+                        transform,
+                        ..default()
+                    },
+                });
+            }
+            None => {
+                println!(
+                    "Just received a fire bullet event for a non extant player: {}",
+                    event.player_id
+                )
+            }
+        };
     }
 }
 
@@ -230,6 +295,38 @@ fn ensure_dummy(
     }
 }
 
+fn spawn_player(
+    mut commands: Commands,
+    mut events: EventReader<PlayerSpawnEvent>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut existing_players: Query<(&Player, &mut Transform)>,
+) {
+    for event in events.read() {
+        match existing_players
+            .iter_mut()
+            .find(|(p, _)| p.id == event.player_id)
+        {
+            Some((_, mut transform)) => {
+                transform.translation = event.position;
+            }
+            None => {
+                commands.spawn(PlayerBundle {
+                    player: Player {
+                        id: event.player_id.clone(),
+                    },
+                    mesh_bundle: MaterialMesh2dBundle {
+                        mesh: meshes.add(shape::Circle::new(50.).into()).into(),
+                        material: materials.add(ColorMaterial::from(Color::BLUE)),
+                        transform: Transform::from_translation(event.position),
+                        ..default()
+                    },
+                });
+            }
+        }
+    }
+}
+
 fn bullet_hit_despawns_dummy(
     mut commands: Commands,
     bullets: Query<(Entity, &Transform), With<Bullet>>,
@@ -249,7 +346,7 @@ fn write_inputs_to_server(
     windows: Query<&Window, With<PrimaryWindow>>,
     cameras: Query<(&Camera, &GlobalTransform)>,
     mouse_button_input: Res<Input<MouseButton>>,
-    players: Query<&Transform, With<Player>>,
+    main_players: Query<(&Transform, &Player), With<MainPlayer>>,
     keyboard_input: Res<Input<KeyCode>>,
     server: Res<NetServer>,
 ) {
@@ -267,20 +364,60 @@ fn write_inputs_to_server(
     let move_left = keyboard_input.pressed(KeyCode::A);
     let move_right = keyboard_input.pressed(KeyCode::D);
 
-    let player = players.get_single().unwrap();
-    let aim_vector = cursor_position - player.translation;
+    let (player_transform, player) = main_players.get_single().unwrap();
+    let aim_vector = cursor_position - player_transform.translation;
 
     let fire_button_pressed = mouse_button_input.just_pressed(MouseButton::Left);
 
-    let input = applesauce::Input {
+    let wrapper = applesauce::Wrapper {
         id: Uuid::new_v4().into(),
-        move_left,
-        move_right,
-        aim_x: aim_vector.x,
-        aim_y: aim_vector.y,
-        fire_button_pressed,
+        inner: Some(Inner::Input(applesauce::Input {
+            player_id: player.id.clone(),
+            move_left,
+            move_right,
+            aim_x: aim_vector.x,
+            aim_y: aim_vector.y,
+            fire_button_pressed,
+            ..Default::default()
+        })),
         ..Default::default()
     };
 
-    server.tx.send(input).unwrap();
+    server.tx.send(wrapper).unwrap();
+}
+
+fn broadcast_state(
+    mut server: ResMut<NetServer>,
+    time: Res<Time>,
+    players: Query<(&Player, &Transform)>,
+) {
+    server.poll_timer.tick(time.delta());
+
+    if !server.poll_timer.finished() {
+        return;
+    }
+
+    let players = players
+        .iter()
+        .map(|(player, transform)| applesauce::Player {
+            id: player.id.clone(),
+            x: transform.translation.x,
+            y: transform.translation.y,
+            ..Default::default()
+        })
+        .collect::<Vec<applesauce::Player>>();
+
+    let state = applesauce::State {
+        players,
+        ..Default::default()
+    };
+
+    server
+        .tx
+        .send(applesauce::Wrapper {
+            id: uuid::Uuid::new_v4().to_string(),
+            inner: Some(Inner::State(state)),
+            ..Default::default()
+        })
+        .unwrap();
 }
--- src/server/mod.rs
+++ src/server/mod.rs
@@ -12,19 +12,19 @@ use crate::protos::generated::applesauce;
 enum Event {
     Disconnect(TcpStream),
     Connect(TcpStream),
-    Input(applesauce::Input),
+    Message(applesauce::Wrapper),
 }
 
 pub(crate) fn serve(
     listen_addr: &str,
     connect_addr: &str,
-) -> (Sender<applesauce::Input>, Receiver<applesauce::Input>) {
+) -> (Sender<applesauce::Wrapper>, Receiver<applesauce::Wrapper>) {
     let connect_addr = connect_addr.to_string();
     let listener = TcpListener::bind(listen_addr).unwrap();
 
     let (tx, rx) = crossbeam_channel::bounded::<Event>(10);
-    let (tx_input, rx_input) = crossbeam_channel::bounded::<applesauce::Input>(10);
-    let (tx_output, rx_output) = crossbeam_channel::bounded::<applesauce::Input>(10);
+    let (tx_input, rx_input) = crossbeam_channel::bounded::<applesauce::Wrapper>(10);
+    let (tx_output, rx_output) = crossbeam_channel::bounded::<applesauce::Wrapper>(10);
 
     let tx2 = tx.clone();
     let rx2 = rx.clone();
@@ -58,15 +58,17 @@ pub(crate) fn serve(
                 Event::Connect(stream) => {
                     streams.push(stream);
                 }
-                Event::Input(input) => {
-                    if proxied_events.contains(&input.id) {
+                Event::Message(wrapper) => {
+                    if proxied_events.contains(&wrapper.id) {
                         continue;
                     }
-                    proxied_events.insert(input.id.clone());
+                    proxied_events.insert(wrapper.id.clone());
 
-                    tx_output.send(input.clone()).unwrap();
+                    tx_output.send(wrapper.clone()).unwrap();
                     for mut stream in streams.iter() {
-                        input.write_length_delimited_to_writer(&mut stream).unwrap();
+                        wrapper
+                            .write_length_delimited_to_writer(&mut stream)
+                            .unwrap();
                     }
                 }
             }
@@ -75,7 +77,7 @@ pub(crate) fn serve(
 
     thread::spawn(move || {
         for input in rx_input.iter() {
-            tx2.send(Event::Input(input)).unwrap();
+            tx2.send(Event::Message(input)).unwrap();
         }
     });
 
@@ -96,8 +98,8 @@ fn handle_connection(stream: TcpStream, events_tx: Sender<Event>) {
                 return;
             }
 
-            let input: applesauce::Input = input_stream.read_message().unwrap();
-            events_tx.send(Event::Input(input)).unwrap();
+            let wrapper: applesauce::Wrapper = input_stream.read_message().unwrap();
+            events_tx.send(Event::Message(wrapper)).unwrap();
         }
     });
 }
